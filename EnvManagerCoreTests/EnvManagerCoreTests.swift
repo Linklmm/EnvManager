1d1bd128bb50d19d2545e2e089d302f899bbb974
你的花花我的草草 <35894038+Linklmm@users.noreply.github.com>
2026-06-01 22:41:29 +0800

    feat；更改样式，增加配置文件获取

---
+++ EnvManagerCoreTests/EnvManagerCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnvManagerCoreTests.swift
+//  EnvManagerCoreTests
+//
+//  Created by playboy on 2026/5/31.
+//
+
+import Testing
+@testable import EnvManagerCore
+
+struct EnvManagerCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
